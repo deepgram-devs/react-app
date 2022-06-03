953f1cb5fad670a1683f34f1dc2e06798d97f28d
BekahHW <rhawrot@gmail.com>
2022-06-03 07:18:30 -0400

    Update word choice

--- src/components/Affirmation.js
+++ src/components/Affirmation.js
@@ -2,17 +2,17 @@ import { useState, useRef } from 'react'
 
 
 export default function Affirmation() {
-    const [gratitude, setGratitude] = useState('')
-	const [finalGratitude, setFinalGratitude] = useState(false)
+    const [affirmation, setAffirmation] = useState('')
+	const [finalAffirmation, setFinalAffirmation] = useState(false)
 	const socketRef = useRef(null)
 
 	const handleChange = (e) => {
-		setGratitude(e.target.value)
+		setAffirmation(e.target.value)
 	}
     const handleSubmit = (e) => {
 		e.preventDefault()
 	
-		setFinalGratitude(true)
+		setFinalAffirmation(true)
 	}
 	const activateMicrophone =  ( )  => {
 
@@ -43,7 +43,7 @@ export default function Affirmation() {
 				const transcript = received.channel.alternatives[0].transcript
 				if (transcript) {
 					console.log(transcript)
-					setGratitude(transcript)
+					setAffirmation(transcript)
 				}
 			}
 
@@ -69,7 +69,7 @@ return(
   
   
 <div className='journal-body'>
-{!finalGratitude ? (
+{!finalAffirmation ? (
 				<>
 					<div className='description'>
 						What affirmation do you want to give to yourself today?
@@ -77,14 +77,14 @@ return(
 					<form onSubmit={handleSubmit}>
 						<textarea
 							className='journal-input'
-							value={gratitude}
+							value={affirmation}
 							onChange={handleChange}
 						/>
 						<br />
 						<button
 							type='submit'
 							className='submit-button'
-							disabled={gratitude.length === 0}>
+							disabled={affirmation.length === 0}>
 							Submit
 						</button>
 
@@ -100,7 +100,7 @@ return(
 			) : (
 				<>
 					<h2>Today's Entry</h2>
-					<div className='description'>{gratitude}</div>
+					<div className='description'>{affirmation}</div>
 				</>
 			)}
          </div> 
